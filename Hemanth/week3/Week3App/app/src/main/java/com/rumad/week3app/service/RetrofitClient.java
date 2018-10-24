b86458ac27df17cc8b276af0c5c07d39011728a9
Hemanth <hemuchiluka3@gmail.com>
2018-10-24 12:52:27 -0400

    week 3

---
+++ Hemanth/week3/Week3App/app/src/main/java/com/rumad/week3app/service/RetrofitClient.java
@@ -0,0 +1,23 @@
+package com.rumad.week3app.service;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    static Retrofit sRetrofit = null;
+    static String BASE_URL = "https://api.openweathermap.org/";
+
+    private static   Retrofit getRetrofit() {
+        if (sRetrofit == null) {
+            sRetrofit = new Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl(BASE_URL)
+                    .build();
+        }
+        return sRetrofit;
+    }
+
+    public static WeatherService getService() {
+        return getRetrofit().create(WeatherService.class);
+    }
+}
---
+++ Hemanth/week3/Week3App/app/src/main/java/com/rumad/week3app/service/WeatherService.java
@@ -0,0 +1,20 @@
+package com.rumad.week3app.service;
+
+import com.rumad.week3app.model.WeatherResponse;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface WeatherService {
+     String APP_ID = "d8e7907c99a2a1b5a167553df8bf8fed";
+
+    @GET("/data/2.5/weather")
+    Call<WeatherResponse> getCurrentWeather(@Query("appid") String appid,
+                                            @Query("q") String city,
+                                            @Query("lat") String lat,
+                                            @Query("long") String lng,
+                                            @Query("zip") String zip);
+
+}
